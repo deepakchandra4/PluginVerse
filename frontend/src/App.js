60fdcfce7dde8d820739643ccfb6cf2840e07452
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-04-28 10:00:46 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter , Routes , Route, Navigate } from 'react-router-dom';
-import Footer from './components/main/Footer';
-import Home from './components/main/Home';
-import Login from './components/main/Login';
-import Signup from './components/main/Signup';
-import Main from './components/main';
-import AddProduct from './components/plugins/AddProduct';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./components/main/Footer";
+import Home from "./components/main/Home";
+import Login from "./components/main/Login";
+import Signup from "./components/main/Signup";
+import Main from "./components/main";
+import AddProduct from "./components/plugins/AddProduct";
+import ManageProduct from "./components/plugins/ManageProduct";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navigate to="/main/home"/>}/>
-      <Route path="main" element={<Main/>}>
-        <Route path="home" element={<Home/>}/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main/home" />} />
+        <Route path="main" element={<Main />}>
+          <Route path="home" element={<Home />} />
 
-        <Route path="login" element={<Login/>}/>
+          <Route path="login" element={<Login />} />
 
-        <Route path="signup" element={<Signup/>}/>
+          <Route path="signup" element={<Signup />} />
 
-        <Route path="footer" element={<Footer/>}/>
-      </Route>
+          <Route path="footer" element={<Footer />} />
+        </Route>
 
-
-        <Route path="addproductplugin" element={<AddProduct/>}/>
-    </Routes>
+        <Route path="addproductplugin" element={<AddProduct />} />
+        <Route path="manageproduct" element={<ManageProduct />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/components/plugins/AddProduct.jsx
+++ frontend/src/components/plugins/AddProduct.jsx
@@ -2,14 +2,48 @@ import React from "react";
 
 const AddProduct = () => {
   return (
-    <div>
+    <div className="container">
       <form>
+        <h1 className="text-center ">Add Plugin Product</h1>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
           </label>
           <input
             type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            User Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Unique Number
+          </label>
+          <input
+            type="number"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
