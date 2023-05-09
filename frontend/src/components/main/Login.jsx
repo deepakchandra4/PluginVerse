db5f694332a4b7ceeea233f3f782d2746b0d2530
Deepak Chandra MAurya <44018172+deepakchandra4@users.noreply.github.com>
2023-05-09 10:41:16 +0530

    ..

--- frontend/src/components/main/Login.jsx
+++ frontend/src/components/main/Login.jsx
@@ -1,120 +1,123 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
+import Swal from "sweetalert2";
 
 const Login = () => {
-	return (
-		<section className="vh-100">
-		<div className="container-fluid h-custom">
-		  <div className="row d-flex justify-content-center align-items-center h-100">
-			<div className="col-md-9 col-lg-6 col-xl-5">
-			  <img
-				src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-				className="img-fluid"
-				alt="Sample image"
-			  />
-			</div>
-			<div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-			  <form>
-				<div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-				  <p className="lead fw-normal mb-0 me-3">Sign in with</p>
-				  <button type="button" className="btn btn-primary btn-floating mx-1">
-					<i className="fab fa-facebook-f" />
-				  </button>
-				  <button type="button" className="btn btn-primary btn-floating mx-1">
-					<i className="fab fa-twitter" />
-				  </button>
-				  <button type="button" className="btn btn-primary btn-floating mx-1">
-					<i className="fab fa-linkedin-in" />
-				  </button>
-				</div>
-				<div className="divider d-flex align-items-center my-4">
-				  <p className="text-center fw-bold mx-3 mb-0">Or</p>
-				</div>
-				{/* Email input */}
-				<div className="form-outline mb-4">
-				  <input
-					type="email"
-					id="form3Example3"
-					className="form-control form-control-lg"
-					placeholder="Enter a valid email address"
-				  />
-				  <label className="form-label" htmlFor="form3Example3">
-					Email address
-				  </label>
-				</div>
-				{/* Password input */}
-				<div className="form-outline mb-3">
-				  <input
-					type="password"
-					id="form3Example4"
-					className="form-control form-control-lg"
-					placeholder="Enter password"
-				  />
-				  <label className="form-label" htmlFor="form3Example4">
-					Password
-				  </label>
-				</div>
-				<div className="d-flex justify-content-between align-items-center">
-				  {/* Checkbox */}
-				  <div className="form-check mb-0">
-					<input
-					  className="form-check-input me-2"
-					  type="checkbox"
-					  defaultValue=""
-					  id="form2Example3"
-					/>
-					<label className="form-check-label" htmlFor="form2Example3">
-					  Remember me
-					</label>
-				  </div>
-				  <a href="#!" className="text-body">
-					Forgot password?
-				  </a>
-				</div>
-				<div className="text-center text-lg-start mt-4 pt-2">
-				  <button
-					type="button"
-					className="btn btn-primary btn-lg"
-					style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-				  >
-					Login
-				  </button>
-				  <p className="small fw-bold mt-2 pt-1 mb-0">
-					Don't have an account?{" "}
-					<a href="#!" className="link-danger">
-					  Register
-					</a>
-				  </p>
-				</div>
-			  </form>
-			</div>
-		  </div>
-		</div>
-		<div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-		  {/* Copyright */}
-		  <div className="text-white mb-3 mb-md-0">
-			Copyright © 2020. All rights reserved.
-		  </div>
-		  {/* Copyright */}
-		  {/* Right */}
-		  <div>
-			<a href="#!" className="text-white me-4">
-			  <i className="fab fa-facebook-f" />
-			</a>
-			<a href="#!" className="text-white me-4">
-			  <i className="fab fa-twitter" />
-			</a>
-			<a href="#!" className="text-white me-4">
-			  <i className="fab fa-google" />
-			</a>
-			<a href="#!" className="text-white">
-			  <i className="fab fa-linkedin-in" />
-			</a>
-		  </div>
-		  {/* Right */}
-		</div>
-	  </section>
-	  
-	)
-}
+  const loginForm = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      console.log(values);
+      const res = await fetch("http://localhost:5000/user/authenticate", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-export default Login
+      console.log(res.status);
+
+      if (res.status === 200) {
+        Swal.fire({
+          icon: "success",
+          title: "Nice",
+          text: "You have successfully logged in",
+        });
+      } else if (res.status === 401) {
+        Swal.fire({
+          icon: "error",
+          title: "Error!!",
+          text: "Invalid Credentials",
+        });
+      }
+    },
+  });
+
+  return (
+    <section
+      className="vh-100 gradient-custom"
+      style={{ backgroundImage: `linear-gradient(to right, #6991ff, #da69ff)` }}
+    >
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              className="card bg-dark text-white"
+              style={{ borderRadius: "1rem" }}
+            >
+              <div className="card-body  p-5 text-center text-white">
+                <form onSubmit={loginForm.handleSubmit}>
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p className="text-white-50 mb-5">
+                      Please enter your login and password!
+                    </p>
+                    <div className="form-white mb-4">
+                      <label className="form-label text-light" htmlFor="typeEmailX">
+                        Email
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        value={loginForm.values.email}
+                        onChange={loginForm.handleChange}
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                    <div className=" form-white mb-4">
+                      <label className="form-label text-light" htmlFor="typePasswordX">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        id="password"
+                        value={loginForm.values.password}
+                        onChange={loginForm.handleChange}
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                    <p className="small mb-5 pb-lg-2">
+                      <a className="text-white-50" href="#!">
+                        Forgot password?
+                      </a>
+                    </p>
+                    <button
+                      className="btn btn-outline-light btn-lg px-5"
+                      type="submit"
+                    >
+                      Login
+                    </button>
+                    <div className="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-facebook-f fa-lg" />
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-twitter fa-lg mx-4 px-2" />
+                      </a>
+                      <a href="#!" className="text-white">
+                        <i className="fab fa-google fa-lg" />
+                      </a>
+                    </div>
+                  </div>
+                  <div>
+                    <p className="mb-0">
+                      Don't have an account?{" "}
+                      <a href="#!" className="text-white-50 fw-bold">
+                        Sign Up
+                      </a>
+                    </p>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
--- frontend/src/components/main/Signup.jsx
+++ frontend/src/components/main/Signup.jsx
@@ -1,106 +1,188 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
+import Swal from "sweetalert2";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short!")
+    .max(10, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().required("Please Enter your password"),
+  // .matches(
+  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+  //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character"
+  // )
+  cPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+});
 
 const Signup = () => {
-	return (
-		<section className="vh-100" style={{ backgroundColor: "#eee" }}>
-		<div className="container h-100">
-		  <div className="row d-flex justify-content-center align-items-center h-100">
-			<div className="col-lg-12 col-xl-11">
-			  <div className="card text-black" style={{ borderRadius: 25 }}>
-				<div className="card-body p-md-5">
-				  <div className="row justify-content-center">
-					<div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-					  <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-						Sign up
-					  </p>
-					  <form className="mx-1 mx-md-4">
-						<div className="d-flex flex-row align-items-center mb-4">
-						  <i className="fas fa-user fa-lg me-3 fa-fw" />
-						  <div className="form-outline flex-fill mb-0">
-							<input
-							  type="text"
-							  id="form3Example1c"
-							  className="form-control"
-							/>
-							<label className="form-label" htmlFor="form3Example1c">
-							  Your Name
-							</label>
-						  </div>
-						</div>
-						<div className="d-flex flex-row align-items-center mb-4">
-						  <i className="fas fa-envelope fa-lg me-3 fa-fw" />
-						  <div className="form-outline flex-fill mb-0">
-							<input
-							  type="email"
-							  id="form3Example3c"
-							  className="form-control"
-							/>
-							<label className="form-label" htmlFor="form3Example3c">
-							  Your Email
-							</label>
-						  </div>
-						</div>
-						<div className="d-flex flex-row align-items-center mb-4">
-						  <i className="fas fa-lock fa-lg me-3 fa-fw" />
-						  <div className="form-outline flex-fill mb-0">
-							<input
-							  type="password"
-							  id="form3Example4c"
-							  className="form-control"
-							/>
-							<label className="form-label" htmlFor="form3Example4c">
-							  Password
-							</label>
-						  </div>
-						</div>
-						<div className="d-flex flex-row align-items-center mb-4">
-						  <i className="fas fa-key fa-lg me-3 fa-fw" />
-						  <div className="form-outline flex-fill mb-0">
-							<input
-							  type="password"
-							  id="form3Example4cd"
-							  className="form-control"
-							/>
-							<label className="form-label" htmlFor="form3Example4cd">
-							  Repeat your password
-							</label>
-						  </div>
-						</div>
-						<div className="form-check d-flex justify-content-center mb-5">
-						  <input
-							className="form-check-input me-2"
-							type="checkbox"
-							defaultValue=""
-							id="form2Example3c"
-						  />
-						  <label className="form-check-label" htmlFor="form2Example3">
-							I agree all statements in{" "}
-							<a href="#!">Terms of service</a>
-						  </label>
-						</div>
-						<div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-						  <button type="button" className="btn btn-primary btn-lg">
-							Register
-						  </button>
-						</div>
-					  </form>
-					</div>
-					<div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-					  <img
-						src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-						className="img-fluid"
-						alt="Sample image"
-					  />
-					</div>
-				  </div>
-				</div>
-			  </div>
-			</div>
-		  </div>
-		</div>
-	  </section>
-	  
-	)
-}
+  const signupForm = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      cPassword: "",
+    },
+    onSubmit: async (values, { setSubmitting }) => {
+      // setSubmitting(true);
+      console.log(values);
 
-export default Signup
+      const res = await fetch("http://localhost:5000/user/add", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(res.status);
+
+      if(res.status === 200){
+
+        Swal.fire({
+          icon : 'success',
+          title : 'Nice',
+          text : 'You have successfully registered'
+        })
+
+      }else{
+        Swal.fire({
+          icon : 'error',
+          title : 'Oops!!',
+          text : 'Something went wrong'
+        })
+      }
+    },
+    validationSchema: SignupSchema,
+  });
+
+  return (
+    <section
+      className="vh-100 bg-image"
+      style={{
+        backgroundImage:
+          'url("https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp")',
+      }}
+    >
+      <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+              <div className="card" style={{ borderRadius: 15 }}>
+                <div className="card-body p-5">
+                  <h2 className="text-uppercase text-center mb-5">
+                    Create an account
+                  </h2>
+                  <form onSubmit={signupForm.handleSubmit}>
+                    <div className=" mb-4">
+                      <label className="form-label" htmlFor="form3Example1cg">
+                        Your Name
+                      </label>
+                      <input
+                        type="text"
+                        id="name"
+                        value={signupForm.values.name}
+                        onChange={signupForm.handleChange}
+                        className="form-control form-control-lg"
+                      />
+                      <span className="text-danger">
+                        {signupForm.errors.name}
+                      </span>
+                    </div>
+                    <div className=" mb-4">
+                      <label className="form-label" htmlFor="form3Example3cg">
+                        Your Email
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        value={signupForm.values.email}
+                        onChange={signupForm.handleChange}
+                        className="form-control form-control-lg"
+                      />
+                      <span className="text-danger">
+                        {signupForm.errors.email}
+                      </span>
+                    </div>
+                    <div className=" mb-4">
+                      <label className="form-label" htmlFor="form3Example4cg">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        id="password"
+                        value={signupForm.values.password}
+                        onChange={signupForm.handleChange}
+                        className="form-control form-control-lg"
+                      />
+                      <span className="text-danger">
+                        {signupForm.errors.password}
+                      </span>
+                    </div>
+                    <div className=" mb-4">
+                      <label className="form-label" htmlFor="form3Example4cdg">
+                        Repeat your password
+                      </label>
+                      <input
+                        type="password"
+                        id="cPassword"
+                        value={signupForm.values.cPassword}
+                        onChange={signupForm.handleChange}
+                        className="form-control form-control-lg"
+                      />
+                      <span className="text-danger">
+                        {signupForm.errors.cPassword}
+                      </span>
+                    </div>
+                    <div className="form-check d-flex justify-content-center mb-5">
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                        defaultValue=""
+                        id="form2Example3cg"
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="form2Example3g"
+                      >
+                        I agree all statements in{" "}
+                        <a href="#!" className="text-body">
+                          <u>Terms of service</u>
+                        </a>
+                      </label>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                      <button
+                        type="submit"
+                        className="btn btn-success btn-block btn-lg gradient-custom-4"
+                        disabled={signupForm.isSubmitting}
+                      >
+                        {signupForm.isSubmitting && (
+                          <span className="spinner-border spinner-border-sm"></span>
+                        )}
+                        &nbsp;&nbsp;Register
+                      </button>
+                    </div>
+                    <p className="text-center text-muted mt-5 mb-0">
+                      Have already an account?{" "}
+                      <a href="#!" className="fw-bold text-body">
+                        <u>Login here</u>
+                      </a>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
